9cb792561158e9cb6f9da8ef5bf6d6e6a13ef645
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-07-09 14:21:52 +0530

    jio cinema package write

--- jio-cinema/forge.config.js
+++ jio-cinema/forge.config.js
@@ -1,32 +1,45 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
   packagerConfig: {
     asar: true,
+    // executableName: "Jio Cinema",
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        options: {
+          icon: "./src/icon.png",
+        },
+      },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {},
+      name: "@electron-forge/maker-deb",
+      config: {
+        options: {
+          icon: "./src/icon.png",
+        },
+      },
     },
     {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      name: "@electron-forge/maker-dmg",
+      config: {
+        options: {
+          icon: "./src/icon.png",
+        },
+      },
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
     // Fuses are used to enable/disable various Electron functionality
